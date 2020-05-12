6518014b692daee9f9e036076f6482ba12812cd0
Harsh <raptor419heavy@gmail.com>
2020-05-12 18:09:24 +0530

    upload

--- app/models.py
+++ app/models.py
@@ -7,5 +7,14 @@ Copyright (c) 2019 - present AppSeed.us
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# from django.contrib.auth.models import AbstractUser
+
+# class ShopUser(AbstractUser):
+#     shopid = models.TextField(max_length=30, blank=True)
+#     location = models.TextField(max_length=500, blank=True)
+#     stop_established = models.DateField(null=True, blank=True)
+
+# class Ration(models.Model):
+#     type = models.CharField(max_length=30)
+#     quantity = models.CharField(max_length=30)
 
